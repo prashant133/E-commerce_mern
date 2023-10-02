e42038f79d8fb1a38e72e5647a50295c8d07a0bb
prashant133 <dsrprasant@gmail.com>
2023-10-02 15:05:21 +0545

    user model and db connectiong

---
+++ helpers/authHelper.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt" )
+
+const hashedPassword = async(password)=> {
+    try {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password , saltRounds)
+        return hashedPassword;
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const comparePassword = async(password, hashedPassword)=>{
+    try {
+       return bcrypt.compare(password, hashedPassword)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+module.exports = {hashedPassword, comparePassword}
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const morgan = require('morgan')
+const authRoutes = require('./routes/authRoutes')
+
+
+
+const app = express()
+
+// loading the env data
+db_conn = process.env.MONGO_URL
+port = process.env.PORT
+// console.log(db_conn)
+// console.log(port)
+
+
+// middleware
+app.use(express.json())
+app.use(morgan('dev'))
+
+// initial routes
+app.get('/',(req , res , next)=> {
+    res.send("servering is running");
+})
+
+// Routes
+app.use('/api/v1/auth', authRoutes)
+
+
+
+// connecting to the mongodb
+mongoose
+    .connect(db_conn)
+    .then(()=>{
+        app.listen(port, ()=>{
+            console.log(`server running on port ${port}`)
+        })
+    }).catch((err)=>{
+        console.log(err)
+    })
