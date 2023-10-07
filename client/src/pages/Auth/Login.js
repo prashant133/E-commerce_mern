8fb456c9f53ec0cc0fd2d24fa956d45bbc99e144
prashant133 <dsrprasant@gmail.com>
2023-10-07 15:47:13 +0545

    login and connected to api

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -6,14 +6,12 @@ import { useNavigate } from "react-router-dom";
 import "../../styles/AuthStyles.css";
 
 
-const Register = () => {
+const Login = () => {
 
-    // using state for register
-    const [name, setName] = useState("") // we have get and set function in useState
+    // using state for login
+   // we have get and set function in useState
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [phone, setPhone] = useState("")
-    const [address, setAddress] = useState("")
 
     const navigate = useNavigate(); //we create a variable becuase it is the hook
 
@@ -23,13 +21,13 @@ const Register = () => {
         e.preventDefault()
         try {
             const res = await Axios.post(
-                `/api/v1/auth/register`,
-                {name , email , password , phone , address}
+                `/api/v1/auth/login`,
+                {email , password}
             );
             // we are checking it from the server side res.data.success
             if(res && res.data.success){
                 toast.success(res.data.message)
-                navigate("/login");
+                navigate("/");
             } else {
                 toast.error(res.data.message)
             }
@@ -42,23 +40,11 @@ const Register = () => {
     }
 
     return (
-        <Layout title={"Register"}>
+        <Layout title={"Login"}>
             <div className="form-container">
-                <h1>Register page</h1>
+                <h1>Login page</h1>
                 <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-
-                        <input
-                            type="text"
-                            value={name}
-                            onChange={(e)=>setName(e.target.value)}
-                            className="form-control"
-                            id="exampleInputName"
-                            placeholder="Enter your name" 
-                            required
-                            />
-
-                    </div>
+                   
 
                     <div className="mb-3">
 
@@ -85,34 +71,10 @@ const Register = () => {
                         required
                         />
                     </div>
-                    <div className="mb-3">
-
-                        <input 
-                        type="text" 
-                        value={phone} 
-                        onChange={(e)=>setPhone(e.target.value)}
-                        className="form-control"
-                        id="exampleInputPhone"
-                        placeholder="Enter your phone No." 
-                        required
-                          />
-
-                    </div>
-                    <div className="mb-3">
-
-                        <input type="text"
-                         value={address} 
-                         onChange={(e)=>setAddress(e.target.value)}
-                         className="form-control"
-                          id="exampleInputAddress" 
-                          placeholder="Enter your Address"
-                          required
-                          />
-
-                    </div>
+                   
 
 
-                    <button type="submit" className="btn btn-primary">Register</button>
+                    <button type="submit" className="btn btn-primary">Login</button>
                 </form>
 
 
@@ -121,4 +83,4 @@ const Register = () => {
     )
 }
 
-export default Register;
+export default Login;
