470d9618835ab9c323fad38dc42424504aecb342
prashant133 <dsrprasant@gmail.com>
2023-10-11 10:45:06 +0545

    product duct model crated

---
+++ models/productModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    name :{
+        type  : String,
+        required : true,
+    },
+    slug : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true,
+    },
+    price : {
+        type : Number,
+        required : true,
+
+    },
+    category : {
+        type : mongoose.ObjectId,
+        ref : 'Category',
+        required  : true
+    },
+    quantity : {
+        type : Number,
+        required : true
+    },
+    photo : {
+        type : Buffer,
+        contentType : String
+    },
+    shipping : {
+        type : Boolean,
+
+    }
+       
+    
+
+},{timestamps : true})
+
+const Product = mongoose.model('Products', productSchema)
+
+module.exports = Product
+
