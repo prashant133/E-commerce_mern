3fe6340122712f181a0f9409113b1736eef53672
prashant133 <dsrprasant@gmail.com>
2023-10-11 12:46:31 +0545

    product delete functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -120,5 +120,67 @@ const productPhotoController = async(req, res)=> {
 
 }
 
+// delete the product
 
-module.exports = {createProductController,getProductController,getSingleProductController,productPhotoController}
+const deleteProductController = async(req,res)=>{
+    try {
+        await Product.findByIdAndDelete(req.params.pid).select("-photo")
+        res.status(200).send({
+            success : true,
+            message : "product deleted successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success : false,
+            message : "Error in deleting the Product",
+            error : message.error,
+        })
+    }
+}
+
+const updateProductController = async(req, res)=> {
+    try {
+        // using express-formidable for form data i.e photo
+        const {name, slug, description, price , category, quantity, shipping} = req.fields;
+        const {photo}= req.files
+
+        // validate
+
+        if(!name  || !description || !price || !category || !quantity  ) {
+            return res.status(500).send({
+                success :false,
+                error : "Provide the credintials"
+
+            })
+        }
+        if (photo && photo.size > 1000000) {
+            return res.status(500).send({
+                error: "must be less than 1MB"
+            });
+        }
+
+        const products = await Product.findByIdAndUpdate(req.params.pid,{...req.fields,slug : slugify(name)},{new : true});
+        if(photo){
+            products.photo.data = fs.readFileSync(photo.path);
+            products.photo.contentType = photo.type
+        }
+        await products.save()
+        res.status(200).send({
+            success : true,
+            message : "Product create successfully",
+            products
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success : false,
+            message : "Error in creating the product",
+            error
+        })
+    }
+}
+
+
+module.exports = {createProductController,getProductController,getSingleProductController,productPhotoController,deleteProductController,updateProductController}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createProductController, getProductController, getSingleProductController, productPhotoController } = require('../controllers/productController')
+const { createProductController, getProductController, getSingleProductController, productPhotoController, deleteProductController, updateProductController } = require('../controllers/productController')
 const { requireSignIn, isAdmin } = require('../middleware/authMiddleware')
 const formidable = require('express-formidable')
 
@@ -7,15 +7,21 @@ const router = express.Router()
 
 // routes
 // create product
-router.post('/create-product',requireSignIn,isAdmin,formidable(),createProductController)
+router.post('/create-product', requireSignIn, isAdmin, formidable(), createProductController)
 
 // get-product
-router.get('/get-product',getProductController)
+router.get('/get-product', getProductController)
 
 // get single product
-router.get('/get-product/:slug',getSingleProductController)
+router.get('/get-product/:slug', getSingleProductController)
 
 // getting product photo
-router.get('/product-photo/:pid',productPhotoController)
+router.get('/product-photo/:pid', productPhotoController)
+
+// delete the product
+router.delete('/delete-product/:pid', requireSignIn, isAdmin, deleteProductController)
+
+//update the product
+router.put('/update-product/:pid', requireSignIn, isAdmin, formidable(), updateProductController)
 
 module.exports = router
