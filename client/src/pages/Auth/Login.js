5f406ce4270d830261c72485022bc166b7a188ae
prashant133 <dsrprasant@gmail.com>
2023-11-28 21:38:41 +0545

    Account Block if password is incorrect 3rd time

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -5,86 +5,118 @@ import { useNavigate, useLocation } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
 import { useAuth } from "../../context/auth";
+
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [auth, setAuth] = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
+  const [loginAttempts, setLoginAttempts] = useState(0);
+  const [isBlocked, setIsBlocked] = useState(false);
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    // form function
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await Axios.post("/api/v1/auth/login", {
-                email,
-                password,
-            });
-            if (res && res.data.success) {
-                toast.success(res.data && res.data.message);
-                setAuth({
-                    ...auth,
-                    user: res.data.user,
-                    token: res.data.token,
-                });
-                localStorage.setItem("auth", JSON.stringify(res.data));
-                navigate(location.state || "/");
-            } else {
-                toast.error(res.data.message);
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error("Something went wrong");
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if the account is blocked
+    if (isBlocked) {
+      toast.error("Account is blocked. Too many login attempts.");
+      return;
+    }
+
+    try {
+      const res = await Axios.post("/api/v1/auth/login", {
+        email,
+        password,
+      });
+
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        navigate(location.state || "/");
+      } else {
+        toast.error(res.data.message);
+
+        // Increment login attempts on failed login
+        setLoginAttempts((prevAttempts) => prevAttempts + 1);
+
+        // Check if the user should be blocked
+        if (loginAttempts + 1 >= 3) {
+          setIsBlocked(true);
         }
-    };
-    return (
-        <Layout title="Register - Ecommer App">
-            <div className="form-container ">
-                <form onSubmit={handleSubmit}>
-                    <h4 className="title">LOGIN FORM</h4>
+      }
+    } catch (error) {
+      console.log(error);
+      
+      // check if the credentila is wrong
+      if(error.response && error.response.status === 401){
+        toast.error("Invalid email or password");
+      }else if(error.response && error.response.status === 403){
+       
+        setIsBlocked(true);
+      }
 
-                    <div className="mb-3">
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="form-control"
-                            id="exampleInputEmail1"
-                            placeholder="Enter Your Email "
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="form-control"
-                            id="exampleInputPassword1"
-                            placeholder="Enter Your Password"
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <button
-                            type="button"
-                            className="btn btn-primary"
-                            onClick={() => {
-                                navigate("/forgot-password");
-                            }}
-                        >
-                            Forgot Password
-                        </button>
-                    </div>
+      else{
+        toast.error("Something went wrong");
+      }
+    }
+  };
 
-                    <button type="submit" className="btn btn-primary">
-                        LOGIN
-                    </button>
-                </form>
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout title="Register - Ecommer App">
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">LOGIN FORM</h4>
+
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => {
+                navigate("/forgot-password");
+              }}
+            >
+              Forgot Password
+            </button>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            LOGIN
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
 };
 
 export default Login;
